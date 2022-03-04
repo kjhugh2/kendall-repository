4d4786416ca42fb98ede8bc2dd08d099903d82b0
Kendall Hughes <kjhugh2@gmail.com>
2022-03-04 17:47:50 -0600

    my second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleText() {
+    var text = document.getElementById("myText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+  
